8bba7c57edd8c1017ed3bff386f55fdccc95ed8c
Akromjon Abdaliev <akromjon996@gmail.com>
2025-12-31 09:43:22 +0000

    add first views and register urls of the app

--- playground/views.py
+++ playground/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def greeting(request) -> HttpResponse:
+    return HttpResponse("Hello, from the playground!")
