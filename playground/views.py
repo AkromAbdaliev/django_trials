e3909f19f4a18a47412c83c312cb905a104a913b
Akromjon Abdaliev <akromjon996@gmail.com>
2026-01-01 11:40:12 +0000

    add ORM trials for fetching data from db

--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from store.models import Product
 
 
 def greeting(request) -> HttpResponse:
-    return render(request, 'index.html', {'name': 'Cybernetic'})
+    query_set = Product.objects.filter(title__icontains='o')
+    return render(request, 'index.html', {'name': 'Cybernetic', 'products': list(query_set)})
